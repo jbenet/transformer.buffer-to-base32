d594600884fd80013a139475e897a1c90a380775
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 04:12:29 -0700

    new version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var transformer = require('dat-transformer');
-var tBuffer = transformer('buffer');
-var tBase32 = transformer('base32');
+var Conversion = require('transformer-conversion');
+var tBuffer = require('transformer.buffer');
+var tBase32 = require('transformer.base32');
 // require any other modules you may need here.
 var base32 = require('base32')
 
-module.exports = transformer.Conversion(tBuffer, tBase32, convert);
+module.exports = Conversion(tBuffer, tBase32, convert);
 
 // this is a synchronous conversion.
 function convert(buffer) {
